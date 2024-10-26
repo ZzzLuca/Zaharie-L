06100bec8c9164065952b185b53a4858df8c8693
ZzzLuca <Zaharie.Lu.Luca@student.utcluj.ro>
2024-10-26 18:09:30 +0300

    Add files via upload

---
+++ Tema 2.py
@@ -0,0 +1,6 @@
+txt = ["Judecătorii din Iași au eliminat toate înregistrările DNA din dosarul lui Dumitru Buzatu, acuzat de luare de mită. Ce urmează"]
+x = ["JudecătoriidinIașiaueliminattoateînregistrărileDNAdin"]
+x = txt.upper ("")
+y = ["Dosarul lui Dumitru Buzatu acuzat de luare de mită Ce urmează"]
+y.reverse("")
+print (x + y)
