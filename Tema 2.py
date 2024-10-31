b3efc7d45c17ca216eb77ee9491b0d10d9ef7af9
ZzzLuca <Zaharie.Lu.Luca@student.utcluj.ro>
2024-10-31 16:17:05 +0200

    Tema 2

--- Tema 2.py
+++ Tema 2.py
@@ -1,6 +1,9 @@
 txt = ["Judecătorii din Iași au eliminat toate înregistrările DNA din dosarul lui Dumitru Buzatu, acuzat de luare de mită. Ce urmează"]
-x = ["JudecătoriidinIașiaueliminattoateînregistrărileDNAdin"]
-x = txt.upper ("")
-y = ["Dosarul lui Dumitru Buzatu acuzat de luare de mită Ce urmează"]
-y.reverse("")
-print (x + y)
+txt1 = txt[len(txt)//2:] 
+txt1.upper()
+txt1.replace(" "," ")
+txt2 = txt[len(txt)//2]
+txt2.replace(".",",","!","?")
+txt2.reverse()
+txt2.capitalize()
+print(txt1 + txt2)
